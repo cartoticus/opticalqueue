01ab3b80e870417b13a369e1adf10917c03afdfd
Cartoticus <46455199+cartoticus@users.noreply.github.com>
2019-01-26 09:37:05 -0500

    hh

--- scripts.js
+++ scripts.js
@@ -1,33 +1,8 @@
-// objects are entries//
-function Name(name, type, time, note, status) {
-  this.name = name;
-  this.type = type;
-  this.time = time;
-  this.note = note;
-  this.status = status;
-  this.speak = function() {
-    console.log("Hi, I'm " + this.name + "!");
-  }
-}
-var p1 = new Name("Robin", 42, 1.72);
-console.log(p1.name);
+  var customerName="";
+  var customerService="";
+  var customerArrival="";
+  var customerNote="";
+  var customerStatus="";
 
-var total = 460;
-var balance = 400;
-var voucher = 100;
-if (voucher > 0) {
-  total = total - voucher;
-}
-if (total > balance) {
-  console.log ("Unsuccessful!")
-}
-if (total < balance) {
-  console.log ("Successful")
-}
-
-var password = "Sturgeon";
-if (password == "Swordfish"){
-  console.log ("Welcome");
-} else {
-  console.log ("Wrong Password")
-}
+  var customer [name, service, arrival, note, status]
+  
